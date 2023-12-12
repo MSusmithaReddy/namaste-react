9346c9f79dfa507639bc5b072bf8de9a9538c6d9
Susmitha Reddy <susmitha.reddy@plobalapps.com>
2023-12-12 23:51:56 +0530

    First session episode 1

---
+++ App.js
@@ -0,0 +1,9 @@
+const heading = React.createElement(
+  "h1",
+  {
+    id: "heading_new",
+  },
+  "Hello React!11"
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
